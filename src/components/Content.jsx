e4fa1646f7b4546fa3632015d0b81aeaf9a28fed
TravisLEUNG <mhl.travis@gmail.com>
2022-09-10 14:31:49 -0400

    add project 2

---
+++ src/components/Content.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Content() {
+  return (
+    <div className="content">
+      <div>
+        <h3>About</h3>
+        <p>
+          Self-motivated and user-centered full-stack developer with 3+ years of
+          experience building sustainable and goal-oriented platform solutions.
+          Proven track records in delivering effective web applications, API
+          systems, and implementing business requirements for robust systems. He
+          has a genuine passion for supporting helpful products that solve our
+          daily hustles with technology.
+        </p>
+      </div>
+      <div>
+        <h3>Interests</h3>
+        <p>
+          <span className="bold">Hiking</span> ninja, <span className="bold">Wakeboard</span> buff. trying to learn <span className="bold">Music</span>. Gym. <span className="bold">Travel</span> geek.
+          tasty <span className="bold">Food</span> finder.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Photo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Img from "../assets/tt-grey9.png"
+
+export default function Photo() {
+    return (<div className="photo">
+        <img src={Img} alt="photo" />
+    </div>);
+}
